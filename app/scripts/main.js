533a3a2aade5c680078f68b1a1355e705114a9c8
yuhualee <550807257@qq.com>
2015-05-19 19:40:33 +0800

    index

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,49 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
+'use strict';
+/*global $:false, navigator:false, Swipe:false,document:false,window:false,CloseDiv:false,ShowDiv:false, */
+;(function(){
+	var mySwipe = new Swipe(document.getElementById('swipeSlide'), {
+	    speed: 400,
+	    auto: 3000,
+	    continuous:true,
+	    callback: function(index) {
+	        $($('#point>i').removeClass('active').get(index)).addClass('active');
+	    }
+	});
+	// point
+	for(var i = 0; i<$('.slide-ul li').length; i++){
+	    var str = $("<i />",{class:"icon-point"});
+	    $(".slide-point").append(str);
+	}
+
+	$('#point>i').eq(0).addClass('active');
+	$('#point>i').each(function(i){
+	    $(this).tap(function(){
+	        mySwipe.slide(i);
+	    });
+	});
+})();
+
+
+//image scale
+;(function(){
+	function imgScale(objs,scale){
+		if(typeof objs == "object" && typeof scale == "number"){
+			$(objs).each(function(){
+				var w = parseInt($(this).width());
+				$(this).height(parseInt(w*scale));
+			});
+		}
+	}
+	// play-story
+	function reset(){
+		imgScale($('.index .slide .slider-li'),0.5);
+	}
+
+	reset();
+	window.onresize = function(){
+		reset();
+	};
+})();
+
+
