eb63550113f78f5665e0f34eb09a561a54c6ddf8
yuhualee <550807257@qq.com>
2015-10-27 18:42:07 +0800

    new

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,34 +1,22 @@
 /* jshint devel:true */
 'use strict';
-/*global $:false, navigator:false, Swipe:false,document:false,window:false,CloseDiv:false,ShowDiv:false, */
-;(function(){
-	var mySwipe = new Swipe(document.getElementById('swipeSlide'), {
-	    speed: 400,
-	    auto: 3000,
-	    continuous:true,
-	    callback: function(index) {
-	        $($('#point>i').removeClass('active').get(index)).addClass('active');
-	    }
-	});
-	// point
-	for(var i = 0; i<$('.slide-ul li').length; i++){
-	    var str = $("<i />",{class:"icon-point"});
-	    $(".slide-point").append(str);
-	}
+/*global $:false, window:false */
 
-	$('#point>i').eq(0).addClass('active');
-	$('#point>i').each(function(i){
-	    $(this).tap(function(){
-	        mySwipe.slide(i);
-	    });
-	});
-})();
+var Module = {
+	pages: {}
+};
+
+Module.pages.fn = function(){
+	return function fn(){
+	};
+}
+
 
 
 //image scale
 ;(function(){
 	function imgScale(objs,scale){
-		if(typeof objs == "object" && typeof scale == "number"){
+		if(typeof objs === 'object' && typeof scale === 'number'){
 			$(objs).each(function(){
 				var w = parseInt($(this).width());
 				$(this).height(parseInt(w*scale));
@@ -37,8 +25,8 @@
 	}
 	// play-story
 	function reset(){
-		imgScale($('.index .slide .slider-li'),0.5);
-		imgScale($('.weekly-star .img'),0.7);
+		imgScale($('.qa .top-banner'),1);
+		// imgScale($('.weekly-star .img'),0.7);
 	}
 
 	reset();
